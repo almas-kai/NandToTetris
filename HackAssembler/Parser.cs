ffb23c3e2526cfd8fd7ec56b5e6f2c63770e3550
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-05-26 21:16:09 +0500

    Parser is ready, though it wasn't tested, which means it is not ready, right?

--- HackAssembler/Parser.cs
+++ HackAssembler/Parser.cs
@@ -10,13 +10,11 @@ public enum InstructionType
 }
 public class Parser
 {
-	private readonly FileInfo _file;
 	private int _counter = 0;
 	private string[] _instructions;
 	public InstructionType InstructionType { get; private set; }
 	public Parser(FileInfo file)
 	{
-		_file = file;
 		using (StreamReader _streamReader = file.OpenText())
 		{
 			_instructions = _streamReader.ReadToEnd()
@@ -30,7 +28,7 @@ public class Parser
 	{
 		if (HasMoreLines is false)
 		{
-			throw new InvalidOperationException("Can't advance. There is no more instructions to read for.");
+			throw new InvalidOperationException($"Invalid operation. Can't advance. There is no more instructions to read for. Current instruction number is {_counter}. Current instruction is {_instructions[_counter]}.");
 		}
 		_counter++;
 		char instructionType = _instructions[_counter][0];
@@ -51,7 +49,7 @@ public class Parser
 	{
 		if (InstructionType == InstructionType.C_INSTRUCTION)
 		{
-			throw new InvalidOperationException($"The instruction type is {InstructionType}. Can't extract the symbol or decimal value.");
+			throw new InvalidOperationException($"Invalid operation. The instruction type is {InstructionType}. But it has to be {InstructionType.C_INSTRUCTION}. Can't extract the symbol or decimal value from this type of instruction. Current instruction number is {_counter}. Current instruction is {_instructions[_counter]}.");
 		}
 		string currentInstruction = _instructions[_counter];
 		if (currentInstruction.StartsWith("(") && currentInstruction.EndsWith(")"))
@@ -63,7 +61,7 @@ public class Parser
 			}
 			else
 			{
-				throw new InvalidOperationException($"The instruction type is {InstructionType}. Couldn't extract the symbol, because the format was incorrect.");
+				throw new InvalidOperationException($"Invalid operation. The instruction type is {InstructionType}. Couldn't extract the symbol, because the format was incorrect. Current instruction number is {_counter}. Current instruction is {_instructions[_counter]}.");
 			}
 		}
 		else if (currentInstruction.StartsWith("@"))
@@ -75,21 +73,156 @@ public class Parser
 			}
 			else
 			{
-				throw new InvalidOperationException($"The instruction type is {InstructionType}. Couldn't extract the underlying symbol or decimal value, because the format was incorrect.");
+				throw new InvalidOperationException($"Invalid operation. The instruction type is {InstructionType}. Couldn't extract the underlying symbol or decimal value, because the format was incorrect. The current instruction number is {_counter}. The current instruction is {_instructions[_counter]}.");
 			}
 		}
 		else
 		{
-			throw new InvalidOperationException($"Something went wrong. The current instruction type is {InstructionType}. The current instruction is: {currentInstruction}.");
+			throw new InvalidOperationException($"Something went wrong. The current instruction type is {InstructionType}. The current instruction number is {_counter}. The current instruction is: {_instructions[_counter]}.");
 		}
 	}
 	public string Destination()
 	{
 		if (InstructionType != InstructionType.C_INSTRUCTION)
 		{
-			throw new InvalidOperationException($"The instruction type is {InstructionType}. Can't extract the destination part.");
+			throw new InvalidOperationException($"Invalid operation. The instruction type is {InstructionType}. But it has to be {InstructionType.C_INSTRUCTION}. Can't extract the destination part from this type of the instruction. The current instruction number is {_counter}. The current instruction is {_instructions[_counter]}.");
 		}
-		
+		string destination = _instructions[_counter].Substring(10, 3);
+		switch (destination)
+		{
+			case "000":
+				return String.Empty;
+			case "001":
+				return "M";
+			case "010":
+				return "D";
+			case "011":
+				return "DM";
+			case "100":
+				return "A";
+			case "101":
+				return "AM";
+			case "110":
+				return "AD";
+			case "111":
+				return "ADM";
+			default:
+				throw new InvalidOperationException($"Something went wrong. The destiantion command is {destination}. The instruction number is {_counter}. The current instruction is {_instructions[_counter]}.");
+		}
+	}
+	public string Computation()
+	{
+		if (InstructionType != InstructionType.C_INSTRUCTION)
+		{
+			throw new InvalidOperationException($"Invalid operation. The instruction type is {InstructionType}. But it has to be {InstructionType.C_INSTRUCTION}. Can't extract the computational part. The current instruction type is {_counter}. The current instruction is {_instructions[_counter]}.");
+		}
+		string computational = _instructions[_counter].Substring(4, 6);
+		char a = _instructions[_counter][3];
+		if (a == '0')
+		{
+			switch (computational)
+			{
+				case "101010":
+					return "0";
+				case "111111":
+					return "1";
+				case "111010":
+					return "-1";
+				case "001100":
+					return "D";
+				case "110000":
+					return "A";
+				case "001101":
+					return "!D";
+				case "110001":
+					return "!A";
+				case "001111":
+					return "-D";
+				case "110011":
+					return "-A";
+				case "011111":
+					return "D+1";
+				case "110111":
+					return "A+1";
+				case "001110":
+					return "D-1";
+				case "110010":
+					return "A-1";
+				case "000010":
+					return "D+A";
+				case "010011":
+					return "D-A";
+				case "000111":
+					return "A-D";
+				case "000000":
+					return "D&A";
+				case "010101":
+					return "D|A";
+				default:
+					throw new InvalidOperationException($"Something went wrong. 'a' is {a}. The command is wrong. Computational command is {computational}. The current instruction number is {_counter}. The current instruction is {_instructions[_counter]}.");
+			}
+		}
+		else if (a == '1')
+		{
+			switch (computational)
+			{
+				case "110000":
+					return "M";
+				case "110001":
+					return "!M";
+				case "110011":
+					return "-M";
+				case "110111":
+					return "M+1";
+				case "110010":
+					return "M-1";
+				case "000010":
+					return "D+M";
+				case "010011":
+					return "D-M";
+				case "000111":
+					return "M-D";
+				case "000000":
+					return "D&M";
+				case "010101":
+					return "D|M";
+				default:
+					throw new InvalidOperationException($"Something went wrong. The 'a' is {a}. The command is wrong. The computational command is {computational}. The current instruction number is {_counter}. The current instruction is {_instructions[_counter]}.");
+			}
+		}
+		else
+		{
+			throw new InvalidOperationException($"Something went wrong. The 'a' instruction is {a}. The computational instruction is {computational}. The instruction number is {_counter}. The current instruction is {_instructions[_counter]}.");
+		}
+	}
+	public string Jump()
+	{
+		if (InstructionType != InstructionType.C_INSTRUCTION)
+		{
+			throw new InvalidOperationException($"Invalid operation. Instruction type error. The current instruction type is {InstructionType}. But it has to be {InstructionType.C_INSTRUCTION}. Can't extract the 'jump' part. The current instruction number is {_counter}. The current instruction is {_instructions[_counter]}.");
+		}
+		string jump = _instructions[_counter].Substring(13);
+		switch (jump)
+		{
+			case "000":
+				return String.Empty;
+			case "001":
+				return "JGT";
+			case "010":
+				return "JEQ";
+			case "011":
+				return "JGE";
+			case "100":
+				return "JLT";
+			case "101":
+				return "JNE";
+			case "110":
+				return "JLE";
+			case "111":
+				return "JMP";
+			default:
+				throw new InvalidOperationException($"Something went wrong. Non-supported 'jump' command. It is {jump}. The current instruction number is {_counter}. The current instruction is {_instructions[_counter]}.");
+		}
 	}
 	public bool HasMoreLines
 	{
