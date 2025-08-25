1b4e6878ec718ac1957db7b093fa5294a7ad7f0b
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-08-25 22:41:06 +0500

    First version of the scheme.

--- JackCompiler/CompilationEngine.cs
+++ JackCompiler/CompilationEngine.cs
@@ -1,3 +1,31 @@
 namespace JackCompiler;
 
-class CompilationEngine {}
+class CompilationEngine
+{
+	private readonly JackTokenizer _jackTokenizer;
+	private readonly string _outputFileName;
+	public CompilationEngine(JackTokenizer jackTokenizer, string outputFileName)
+	{
+		_jackTokenizer = jackTokenizer;
+		_outputFileName = outputFileName;
+		CompileClass();
+	}
+	public void CompileClass() { }
+	public void CompileClassVarDec() { }
+	public void CompileSubroutine() { }
+	public void CompileParameterList() { }
+	public void CompileSubroutineBody() { }
+	public void CompileVarDec() { }
+	public void CompileStatements() { }
+	public void CompileLet() { }
+	public void CompileIf() { }
+	public void CompileWhile() { }
+	public void CompileDo() { }
+	public void CompileReturn() { }
+	public void CompileExpression() { }
+	public void CompileTerm() { }
+	public int CompileExpressionList()
+	{
+		throw new NotImplementedException();
+	}
+}
