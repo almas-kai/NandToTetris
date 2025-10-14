58411e345ba7587bca6589372eb6e6d171d1afc1
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-10-14 19:30:54 +0500

    Add more tests.

--- Jack Compiler/JackCompilerTests/UtilitiesTests/CompilerRegexTests/IsSymbolTests.cs
+++ Jack Compiler/JackCompilerTests/UtilitiesTests/CompilerRegexTests/IsSymbolTests.cs
@@ -35,4 +35,53 @@ public class IsSymbolTests
 
 		Assert.AreEqual(expected, actual, "Correct symbol should be treated as such.");
 	}
+
+	[TestMethod()]
+	[DataRow("{")]
+	[DataRow("}")]
+	[DataRow("(")]
+	[DataRow(")")]
+	[DataRow("[")]
+	[DataRow("]")]
+	[DataRow(".")]
+	[DataRow(",")]
+	[DataRow(";")]
+	[DataRow("+")]
+	[DataRow("-")]
+	[DataRow("*")]
+	[DataRow("/")]
+	[DataRow("&")]
+	[DataRow("|")]
+	[DataRow("<")]
+	[DataRow(">")]
+	[DataRow("=")]
+	[DataRow("~")]
+	public void PassingCorrectSymbol_MatchValueEqualsToThePassedSymbol(string correctSymbol)
+	{
+		string expected = correctSymbol;
+
+		Match match = CompilerRegex.IsSymbol(correctSymbol);
+		string actual = match.Value;
+
+		Assert.AreEqual(expected, actual, "Correct symbol should be treated as such.");
+	}
+
+	[TestMethod()]
+	[DataRow(":")]
+	[DataRow("v")]
+	[DataRow("and")]
+	[DataRow("_")]
+	[DataRow("$")]
+	[DataRow("")]
+	[DataRow("!")]
+	[DataRow("@")]
+	public void PassingIncorrectSymbol_MatchSuccessIsFalse(string incorrectSymbol)
+	{
+		bool expected = false;
+
+		Match match = CompilerRegex.IsSymbol(incorrectSymbol);
+		bool actual = match.Success;
+
+		Assert.AreEqual(expected, actual, "Incorrect symbol shouldn't have succesful match.");
+	}
 }
