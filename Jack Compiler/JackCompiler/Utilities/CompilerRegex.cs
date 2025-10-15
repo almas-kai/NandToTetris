9396a5a47fe95b4093fd7fe2594732b6f14627f4
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-10-15 19:26:55 +0500

    Internal naming refactor.

--- Jack Compiler/JackCompiler/Utilities/CompilerRegex.cs
+++ Jack Compiler/JackCompiler/Utilities/CompilerRegex.cs
@@ -7,7 +7,7 @@ internal static class CompilerRegex
 	private static readonly Regex _commentsRegex = new Regex(@"//.*|/\*[\s\S]*?\*/", RegexOptions.Compiled);
 	private static readonly Regex _keywordRegex = new Regex(@"\b(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)\b", RegexOptions.Compiled);
 	private static readonly Regex _symbolRegex = new Regex(@"[{}()\[\].,;+\-*/&|<>=~]", RegexOptions.Compiled);
-	private static readonly Regex _integerConstantRegex = new Regex(@"\b\d+\b", RegexOptions.Compiled);
+	private static readonly Regex _positiveIntegerConstantRegex = new Regex(@"\b\d+\b", RegexOptions.Compiled);
 	private static readonly Regex _stringConstantRegex = new Regex(@"(""[^""]*"")", RegexOptions.Compiled);
 	private static readonly Regex _identifierRegex = new Regex(@"\b([_a-zA-Z]+[_a-zA-Z0-9]*)\b", RegexOptions.Compiled);
 	private static readonly Regex _spaceRegex = new Regex(@"\s", RegexOptions.Compiled);
@@ -25,7 +25,7 @@ internal static class CompilerRegex
 	}
 	public static Match IsPositiveIntegerConstant(string instruction)
 	{
-		return _integerConstantRegex.Match(instruction);
+		return _positiveIntegerConstantRegex.Match(instruction);
 	}
 	public static Match IsStringConstant(string instruction)
 	{
